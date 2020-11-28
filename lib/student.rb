e7cf0624ab636cb8e3453f700949f58a9480c049
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-28 14:08:38 -0800

    commit tree 651c12a20c32316d88cac03920a0fda51173be46

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,31 @@
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  @@all = []
+  def initialize(name, grade, id=nil)
+    @name = name 
+    @grade = grade 
+    @id = id 
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+      );"
+    DB[:conn].execute(sql)
+    end
+
+  end 
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
